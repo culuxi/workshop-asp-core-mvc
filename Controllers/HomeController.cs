b9340f065869f23b9461d0e1d1027c3640137490
Diego Culuxi <culuxi@gmail.com>
2019-10-08 10:16:24 -0300

    First Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Diego Alves Culuxi";
+            ViewData["Email"] = "culuxi@gmail.com";
 
             return View();
         }
